99dfbb227129a28a8943e7a604f00a24067e19e7
Salma Aqarrout <salma.aqarrout@kcl.ac.uk>
2023-10-12 12:57:01 +0100

    Attempted second exercise.

--- things/models.py
+++ things/models.py
@@ -3,5 +3,8 @@ from django.db import models
 # Create your models here.
 
 class Thing(models.Model):
-    pass
+    name = models.CharField(max_length=100)  
+    description = models.TextField()  
+    quantity = models.IntegerField()
 
+    
